ae6fd65c38f5c2c07ad378c7b8d6e47e5dbf988a
wanghai25 <423822728@qq.com>
2017-03-02 17:32:58 +0800

    压缩所有静态库js

--- fis3/fis-conf.js
+++ fis3/fis-conf.js
@@ -20,6 +20,9 @@ fis.hook('commonjs', {
 
 //components下面的所有js资源都是组件化资源
 
+// 改用 npm 方案，而不是用 fis-components
+fis.hook('node_modules');
+
 fis.match('/components/**.js', {
 	isMod: true
 });
@@ -132,7 +135,11 @@ fis.media('web').match('**', {
 		})
 	]
 })
-
+//除module的全部压缩
+fis.match(/[^module\/]\/(.*)\.js/, {
+	// fis-optimizer-uglify-js 插件进行压缩，已内置
+	optimizer: fis.plugin('uglify-js')
+});
 fis.media('web').match('*.js', {
 	// fis-optimizer-uglify-js 插件进行压缩，已内置
 	optimizer: fis.plugin('uglify-js')
